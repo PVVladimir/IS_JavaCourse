599bcbdd14e64d241e918791cb7bddf879d11413
PVVladimir <PVVladimir@mail.ru>
2022-03-24 14:34:02 +0300

    Delete target dir from git. Edit home work

--- HomeWork01-AGlassThatsHalfFull/src/main/java/pvv/project/AGlassThatsHalfFull.java
+++ HomeWork01-AGlassThatsHalfFull/src/main/java/pvv/project/AGlassThatsHalfFull.java
@@ -2,14 +2,11 @@ package pvv.project;
 
 public class AGlassThatsHalfFull {
     public static void main(String[] args) {
-        int volumeGlass;
-        double percentFillGlass;
-        int volumeFillGlass;
-        volumeGlass = 250;
-        volumeFillGlass = 200;
-        percentFillGlass = (double) volumeFillGlass / (double) volumeGlass;
+        int volumeGlass = 250;
+        double percentFillGlass = 81.3;
+        var volumeFillGlass = volumeGlass * percentFillGlass / 100;
         System.out.printf("Объем стакана %d мл.\n", volumeGlass);
-        System.out.printf("В стакан налили %d мл.\n", volumeFillGlass);
         System.out.printf("Стакан наполнен на %.2f%%.\n", percentFillGlass);
+        System.out.printf("В стакан налили %.2f мл.\n", volumeFillGlass);
     }
 }
