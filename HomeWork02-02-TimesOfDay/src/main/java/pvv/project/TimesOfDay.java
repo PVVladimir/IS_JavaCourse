0c165929727f5320dd511bfb181001cef2bec639
PVVladimir <PVVladimir@mail.ru>
2022-03-29 01:12:50 +0300

    Home Work 02: 1-4 labs

---
+++ HomeWork02-02-TimesOfDay/src/main/java/pvv/project/TimesOfDay.java
@@ -0,0 +1,16 @@
+package pvv.project;
+
+public class TimesOfDay {
+    public static void main(String[] args) {
+        final int curHour = 11;
+        if (curHour < 5) {
+            System.out.println("Ночь");
+        } else if (curHour < 11){
+            System.out.println("Утро");
+        } else if (curHour < 17){
+            System.out.println("День");
+        } else {
+            System.out.println("Вечер");
+        }
+    }
+}
---
+++ HomeWork02-03-TimeOfDayWithErrorNotifications/src/main/java/pvv/project/TimeOfDayWithErrorNotifications.java
@@ -0,0 +1,20 @@
+package pvv.project;
+
+public class TimeOfDayWithErrorNotifications {
+    public static void main(String[] args) {
+        final int curHour = 22;
+        if (curHour < 0) {
+            System.out.println("Время суток нужно ввести правильно");
+        } else if (curHour < 5) {
+            System.out.println("Ночь");
+        } else if (curHour < 11){
+            System.out.println("Утро");
+        } else if (curHour < 17){
+            System.out.println("День");
+        } else if (curHour > 23){
+            System.out.println("Вам следует вернуться с другой планеты на Землю");
+        } else {
+            System.out.println("Вечер");
+        }
+    }
+}
