f4c1b8badd7426d15ae538efe084e8a65e2bde4d
PVVladimir <PVVladimir@mail.ru>
2022-04-01 19:38:12 +0300

    Home Work 02: 1 fix my dig error!

--- HomeWork02-01-IdealWeight/src/main/java/pvv/project/IdealWeight.java
+++ HomeWork02-01-IdealWeight/src/main/java/pvv/project/IdealWeight.java
@@ -4,7 +4,7 @@ public class IdealWeight {
     public static void main(String[] args) {
         final double personWeight = 49.8;
         final double personHeight = 179.5;
-        if ((personHeight - 100) * 0.90 - 5 <= personWeight && personWeight <=(personHeight - 100) * 0.90 + 5) {
+        if ((personHeight - 100) * 0.90 - 5 <= personWeight && personWeight <= (personHeight - 100) * 0.90 + 5) {
             System.out.println("Идеальный вес");
         }else {
             System.out.println("Не идеальный вес");
